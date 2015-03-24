38024e1f75ef0ade073cc3301c3953bd4e2f95ae
Jakub Karel <jakub.karel.upol@gmail.com>
2015-03-24 23:47:46 +0100

    lecture06 task 3

---
+++ lecture06_3/lecture06_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace lecture06_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Timer timer = new Timer(x => Console.Title = DateTime.Now.ToLongTimeString(), null, 0, 1000);
+            Console.ReadLine();  
+        }   
+    }
+}
