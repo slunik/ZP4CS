9fdecc397e13976fdf6d0e43cfa4579ebe278bbc
Jakub Karel <jakub.karel.upol@gmail.com>
2015-03-25 00:08:17 +0100

    lecture06 - task 4 - Awesome clock window

---
+++ lecture06_4/lecture06_4/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace lecture06_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClock = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelClock
+            // 
+            this.labelClock.AutoSize = true;
+            this.labelClock.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelClock.Location = new System.Drawing.Point(23, 19);
+            this.labelClock.Name = "labelClock";
+            this.labelClock.Size = new System.Drawing.Size(237, 63);
+            this.labelClock.TabIndex = 0;
+            this.labelClock.Text = "00:00:00";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.labelClock);
+            this.Name = "Form1";
+            this.Text = "AwesomeClock";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelClock;
+    }
+}
+
---
+++ lecture06_4/lecture06_4/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lecture06_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Height = labelClock.Height + 80;
+            this.Width = labelClock.Width + 50;
+
+            Timer timer = new Timer();
+            timer.Interval = 1000;          
+            timer.Tick += ((x, y) => labelClock.Text = DateTime.Now.ToLongTimeString());           
+            timer.Start();
+        }      
+    }
+}
